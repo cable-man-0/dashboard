9867669b503d2f81708ed90e6fdabfd87531d233
cable-man-0 <cherif.raed.0@gmail.com>
2024-02-07 20:03:19 +0100

    adding auth

--- src/flask_app/backend.py
+++ src/flask_app/backend.py
@@ -4,10 +4,79 @@ import pandas as pd
 from algorithms.dbscan import detect_anomalies_dbscan
 from algorithms.svm import detect_anomalies_svm
 from algorithms.isolation_forest import detect_anomalies_iforest
-import logging 
+import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+users={}
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        # Check if the content type is JSON
+        if request.headers['Content-Type'] != 'application/json':
+            logging.error('Verify the data format is in JSON')
+            return jsonify({'error': 'Invalid content type. Expected JSON data.'}), 400
+
+        # Get JSON data from request
+        data = request.json
+        if 'username' not in data or 'password' not in data:
+            logging.error('Invalid JSON format. Expected "username" and "password" keys.')
+            return jsonify({'error': 'Invalid JSON format. Expected "username" and "password" keys.'}), 400
+
+        # Extract username and password from data
+        username = data['username']
+        password = data['password']
+
+        # Check if the username is already taken
+        if username in users:
+            logging.error('Username already exists.')
+            return jsonify({'error': 'Username already exists.'}), 400
+
+        # Register the user
+        users[username] = password
+
+        logging.error('User registration successful.')
+        return jsonify({'message': 'User registration successful.'}), 200
+    except Exception as e:
+        logging.error(f'An error occurred: {str(e)}')
+        return jsonify({'error': str(e)}), 500
+
+# Endpoint for user authentication
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    try:
+        # Check if the content type is JSON
+        if request.headers['Content-Type'] != 'application/json':
+            logging.error('Verify the data format is in JSON')
+            return jsonify({'error': 'Invalid content type. Expected JSON data.'}), 400
+
+        # Get JSON data from request
+        data = request.json
+        if 'username' not in data or 'password' not in data:
+            logging.error('Invalid JSON format. Expected "username" and "password" keys.')
+            return jsonify({'error': 'Invalid JSON format. Expected "username" and "password" keys.'}), 400
+
+        # Extract username and password from data
+        username = data['username']
+        password = data['password']
+
+        # Check if the username exists
+        if username not in users:
+            logging.error('Invalid username or password.')
+            return jsonify({'authenticated': False}), 401
+
+        # Check if the password is correct
+        if users[username] != password:
+            logging.error('Invalid username or password.')
+            return jsonify({'authenticated': False}), 401
+
+        logging.error('User authentication successful.')
+        return jsonify({'authenticated': True}), 200
+    except Exception as e:
+        logging.error(f'An error occurred: {str(e)}')
+        return jsonify({'error': str(e)}), 500
+
 
 # Endpoint for anomaly detection
 @app.route('/detect_anomalies', methods=['POST'])
@@ -15,7 +84,7 @@ def detect_anomalies():
     try:
         # Check if the content type is JSON
         if request.headers['Content-Type'] != 'application/json':
-            logging.error('verify the data format is in JSON ')
+            logging.error('Verify the data format is in JSON')
             return jsonify({'error': 'Invalid content type. Expected JSON data.'}), 400
 
         # Get JSON data from request
@@ -26,18 +95,19 @@ def detect_anomalies():
 
         # Convert data to DataFrame
         df = pd.DataFrame(data['data'])
-        threshold = data['thresholds']
+        thresholds = data['thresholds']
+        
         # Select anomaly detection algorithm
         algorithm = data['algorithm']
         if algorithm == 'isolation_forest':
-            anomaly_indices = detect_anomalies_iforest(df, threshold)
+            anomaly_indices = detect_anomalies_iforest(df, thresholds)
             if anomaly_indices is None:
                 logging.error('An error occurred while detecting anomalies with isolation_forest.')
-                return jsonify({'error': 'An error occurred while detecting anomalies with DBSCAN.'}), 500
+                return jsonify({'error': 'An error occurred while detecting anomalies with isolation_forest.'}), 500
         elif algorithm == 'SVM':
             anomaly_indices = detect_anomalies_svm(df)
             if anomaly_indices is None:
-                logging.error('An error occurred while detecting anomalies with svm.')
+                logging.error('An error occurred while detecting anomalies with SVM.')
                 return jsonify({'error': 'An error occurred while detecting anomalies with SVM.'}), 500
         elif algorithm == 'DBSCAN':
             anomaly_indices = detect_anomalies_dbscan(df)
@@ -45,7 +115,7 @@ def detect_anomalies():
                 logging.error('An error occurred while detecting anomalies with DBSCAN.')
                 return jsonify({'error': 'An error occurred while detecting anomalies with DBSCAN.'}), 500
         else:
-            logging.error('invalid algorithm specified')
+            logging.error('Invalid algorithm specified')
             return jsonify({'error': 'Invalid algorithm specified.'}), 400
 
         # Check if anomalies are detected
@@ -54,7 +124,7 @@ def detect_anomalies():
             return jsonify({'error': 'An error occurred while detecting anomalies.'}), 500
 
         # Return anomaly indices
-        logging.error('success')
+        logging.error('Success')
         return jsonify(anomaly_indices), 200
     except Exception as e:
         logging.error(f'An error occurred: {str(e)}')
--- src/streamlit_app/dashboard.py
+++ src/streamlit_app/dashboard.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 import requests
-import matplotlib.pyplot as plt
 
 # Define a function to load data from CSV file with caching
 @st.cache_data()
@@ -11,90 +10,130 @@ def load_data(file_path):
     df = pd.read_csv(file_path)
     if 'date' in df.columns:
         df['date'] = pd.to_datetime(df['date'])
+    df = df.bfill().dropna()  # Preprocess the data once
     return df
 
 # Define a function to detect anomalies with caching
+
 def detect_anomalies(data, algorithm, thresholds=None):
     if thresholds is None:
         thresholds = {}
     response = requests.post('http://127.0.0.1:5000/detect_anomalies', json={'data': data.to_dict(orient='list'), 'thresholds': thresholds, 'algorithm': algorithm})
     return response
 
+# Define a function to validate login credentials
+def authenticate(username,password):
+    response = requests.post('http://127.0.0.1:5000/authenticate', json={'username': username, 'password': password})
+    if response.ok:
+        return response.json()['authenticated']
+    return False
+# Define a function to register a new user
+def register(username, password):
+    response = requests.post('http://127.0.0.1:5000/register', json={'username': username, 'password': password})
+    return response.ok
+
 # Main function
 def main():
     # Set page title and icon
     st.set_page_config(page_title="Anomaly Detection App", page_icon="🔍")
-
+        
     # Title and description
     st.title("Anomaly Detection App")
-    st.write("Upload a CSV dataset and choose the features to run anomaly detection on. You can also set thresholds for anomaly detection for each numerical feature.")
+        
+    # Check if user is authenticated
+    if 'authenticated' not in st.session_state:
+        st.session_state.authenticated = False
 
-    # Load Style css
-    with open('/home/teemo/Desktop/pfe/env/src/streamlit/src/style/style.css') as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    # Login section
+    st.subheader("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    login_button = st.button("Login")
 
-    # Upload CSV dataset
-    uploaded_file = st.file_uploader("Upload CSV dataset", type=["csv"])
+    if login_button:
+        if authenticate(username, password):
+            st.success("Logged in successfully!")
+            st.session_state.authenticated = True
+        else:
+            st.error("Invalid username or password.")
+            st.session_state.authenticated = False
 
-    # Check if a file has been uploaded
-    if uploaded_file is not None:
-        # Load CSV dataset
-        df = load_data(uploaded_file)
+    # Registration section
+    st.subheader("Register")
+    register_username = st.text_input("Create a Username")
+    register_password = st.text_input("Create a Password", type="password")
+    register_button = st.button("Register")
 
-        df = df.bfill().dropna()
+    if register_button:
+        if register(register_username, register_password):
+            st.success("Registered successfully!")
+        else:
+            st.error("Registration failed.")
 
-        # Display dataset
-        st.subheader("Dataset")
-        st.write(df)  # Display the dataset
+    if st.session_state.authenticated:   
+            # Load Style css
+        with open('/home/teemo/Desktop/pfe/env/src/streamlit/src/style/style.css') as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-        # Sidebar for user interaction
-        st.sidebar.title("Settings")
+        # Upload CSV dataset
+        st.write("Upload a CSV dataset and choose the features to run anomaly detection on. You can also set thresholds for anomaly detection for each numerical feature.")
+        uploaded_file = st.file_uploader("Upload CSV dataset", type=["csv"])
 
-        # Select graph type
-        graph_type = st.sidebar.selectbox("Select Graph Type", ["Line Chart", "Scatter Plot"])
-        algorithm = st.sidebar.selectbox("Select Anomaly Detection Algorithm", ["isolation_forest","SVM","DBSCAN"])
-        selected_features = st.sidebar.multiselect("Select Features for Anomaly Detection", df.columns.tolist())
-
-        if algorithm == "isolation_forest":
-            thresholds = {}
-            for feature in selected_features:
-                thresholds[feature] = st.sidebar.slider(f"Threshold for {feature}", min_value=0.0, max_value=100.0, value=50.0)    
-
-        # Confirm button
-        if st.sidebar.button("Confirm"):
-            if selected_features:
-                data = df[selected_features]
-
-                if algorithm == "isolation_forest":
-                    # Detect anomalies
-                    response = detect_anomalies(data, algorithm, thresholds)
-                elif algorithm in ["DBSCAN", "SVM"]:
-                    # Detect anomalies
-                    response = detect_anomalies(data, algorithm)
-
-                if response is not None and response.status_code == 200:
-                    anomaly_indices = response.json()
-                    if anomaly_indices:
-                        st.info("Anomalies detected! See details below.")
-                        #anomaly_data_points = df.loc[anomaly_indices]
-                        st.subheader("Anomaly Data Points")
-                        st.table(pd.DataFrame({'Index': [idx for idx in anomaly_indices if idx < 0]}))
+        # Check if a file has been uploaded
+        if uploaded_file is not None:
+            # Load CSV dataset
+            df = load_data(uploaded_file)
+            
+            # Dsplay dataset
+            st.subheader("Dataset")
+            st.write(df)  # Display the dataset
+            
+            # Sidebar for user interaction
+            st.sidebar.title("Settings")
+            
+            # Select graph type
+            graph_type = st.sidebar.selectbox("Select Graph Type", ["Line Chart", "Scatter Plot"])
+            algorithm = st.sidebar.selectbox("Select Anomaly Detection Algorithm", ["isolation_forest","SVM","DBSCAN"])
+            selected_features = st.sidebar.multiselect("Select Features for Anomaly Detection", df.columns.tolist())
+            
+            if algorithm == "isolation_forest":
+                thresholds = {}
+                for feature in selected_features:
+                    thresholds[feature] = st.sidebar.slider(f"Threshold for {feature}", min_value=0.0, max_value=100.0, value=50.0)    
+            
+            # Confirm button
+            if st.sidebar.button("Confirm"):
+                if selected_features:
+                    data = df[selected_features]  # Use selected features directly
+                    
+                    if algorithm == "isolation_forest":
+                        # Detect anomalies
+                        response = detect_anomalies(data, algorithm, thresholds)
+                    elif algorithm in ["DBSCAN", "SVM"]:
+                        # Detect anomalies
+                        response = detect_anomalies(data, algorithm)
+                    
+                    if response is not None and response.status_code == 200:
+                        anomaly_indices = response.json()
+                        if anomaly_indices:
+                            st.info("Anomalies detected! See details below.")
+                            #anomaly_data_points = df.loc[anomaly_indices]
+                            st.subheader("Anomaly Data Points")
+                            st.table(pd.DataFrame({'Index': [idx for idx in anomaly_indices if idx < 0]}))
+                        else:
+                            st.success("No anomalies detected.")
                     else:
-                        st.success("No anomalies detected.")
-                else:
-                    st.error("An error occurred during anomaly detection.")
+                        st.error("An error occurred during anomaly detection.")
+                    # Visualization based on selected graph type
+                    for column in data.columns:
+                        chart_placeholder = st.empty()
+                        if graph_type == "Line Chart":
+                            fig = px.line(data, x='date', y=column) if 'date' in data.columns else px.line(data, x=data.index, y=column)
+                        elif graph_type == "Scatter Plot":
+                            fig = px.scatter(data, x='date', y=column) if 'date' in data.columns else px.scatter(data, x=data.index, y=column)
+                            
+                        chart_placeholder.plotly_chart(fig)
 
-                # Visualization based on selected graph type
-                for column in data.columns:
-                    chart_placeholder = st.empty()
-
-                    if graph_type == "Line Chart":
-                        fig = px.line(df, x='date', y=column) if 'date' in df.columns else px.line(df, x=df.index, y=column)
-                    elif graph_type == "Scatter Plot":
-                        fig = px.scatter(df, x='date', y=column) if 'date' in df.columns else px.scatter(df, x=df.index, y=column)
-                        
-                    chart_placeholder.plotly_chart(fig)
-
-# Run the app
+# Run the ap
 if __name__ == "__main__":
     main()
